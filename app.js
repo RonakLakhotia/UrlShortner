96fdbbc9898485866ea92d33d0a3451769c57dce
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2018-09-02 17:24:26 +0800

    Setup server

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const app = express();
 const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(cors());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening on port ' + port));
