083dfefb86fc9a078e43beca123761a7309d6fee
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2018-09-02 18:51:21 +0800

    Set up frontend

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/new/:url(*)', (req, res) => {
+	var urlToShorten = req.params.url;
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port ' + port));
