e166565751f42cb7433c17aa468f50366bcc529e
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2018-09-02 19:30:56 +0800

    Remove .DS_STORE

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const button = document.getElementById("short");
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/new/:url(*)', (req, res) => {
 	var urlToShorten = req.params.url;
+	console.log(button);
 });
 
 
