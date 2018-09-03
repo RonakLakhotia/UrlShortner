cbaf41db60005bc3f3b7f5afc71881d25dfad147
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2018-09-03 22:54:26 +0800

    Indent code

--- public/script.js
+++ public/script.js
@@ -8,24 +8,24 @@ $('.btn-shorten').on('click', function(){
 		dataType: 'JSON',
 		data: {url: $('#short').val()},
 		success: function(data) {
-	    
-	    console.log(data);
-	    if (data.shorterUrl != 'Incorrect url expression' && data.shorterUrl != 'Failed to store in Database') {
-		    var resultHTML = '<a class="result" href="' + data.shorterUrl + '" + target="_blank">'
-		        + data.shorterUrl + '</a>';
-		    $('#link').html(resultHTML);
-	        $('#link').hide().fadeIn('slow');    
-	    } else {
-	    	var resultHTML = '<h2>' + data.shorterUrl + '</h2>';
-	    	$('#error').html(resultHTML);
-	        $('#error').hide().fadeIn('slow');
-	    }
+			
+			console.log(data);
+			if (data.shorterUrl != 'Incorrect url expression' && data.shorterUrl != 'Failed to store in Database') {
+				var resultHTML = '<a class="result" href="' + data.shorterUrl + '" + target="_blank">'
+				+ data.shorterUrl + '</a>';
+				$('#link').html(resultHTML);
+				$('#link').hide().fadeIn('slow');    
+			} else {
+				var resultHTML = '<h2>' + data.shorterUrl + '</h2>';
+				$('#error').html(resultHTML);
+				$('#error').hide().fadeIn('slow');
+			}
 
-    },
-    error: function(jqXHR, textStatus, err) {
-    	alert('text status '+textStatus+', err '+err)
-    }
-});
+		},
+		error: function(jqXHR, textStatus, err) {
+			alert('text status '+textStatus+', err '+err)
+		}
+	});
 });
 
 
